62423ea7d9bdc95ca969e1a3fcf9cae3d2b4356d
farhamehamood <114940911+farhamehamood@users.noreply.github.com>
2023-01-06 19:25:26 -0800

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,79 @@
+function login(){
+    var username=document.getElementById('Ussr').value
+    var password=document.getElementById('Pass').value
+    if(username=="farhaMehamood"  &&  password=="1234")
+    {
+location.assign("http://127.0.0.1:5500/cloneWebsites/finalFirstcryweb.html");
+    }
+    else{
+        window.alert("login failed");
+    }
+}
+
+
+//practice
+const numbers=[1,2,3,4,5,6,7,8,9,10]
+const a = {
+    name : "FARHA",
+    age : 27,
+    contact : 7026393621,
+    education :[{
+           "M.tech" : 2019, 
+             "B.E" : 2017}]
+           ,
+    techstack : ["HTML","CSS", "JS", "JAVA", "SPRINGBOOT", "MYSQL"],
+    gender : "female",
+    }
+const b ={
+
+        Name:" Arbaz Nandgaon",
+        Age: "22",
+        Contact:9380610250,
+        LinkedIn : "arbaznandgaon//linkedIn",
+        Education:[{
+            School: "S.G.E.T", PUCollage: "K.L.E", Univercity: "VTU" }],
+        TechStack: "HTML_CSS, JS",
+        Gender: "Male"
+        }
+const c = {
+            name : "sumera",
+            age : 22,
+            contact : 987654321,
+            education :[{
+                     "B.E" : 2021}]
+                   ,
+            techstack : ["HTML","CSS", "JS", "JAVA", "MYSQL"],
+            gender : "FEMALE",
+           };
+
+           const array=[a,b,c]
+           console.log(array);
+
+
+const num=[
+    [45,65],
+    [67,90],
+    [76,89]
+]
+
+
+
+var balance = document.getElementById ('bankBalance').value; 
+balance = Number (balance); var withdraw = document.getElementById ('withdraw').deposite;
+
+
+
+
+
+
+
+
+
+1
+
+
+
+
+
+
+
